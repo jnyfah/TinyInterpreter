1d5bc3aec6a48dd716d553c5dc8f6a536808b6c5
Jennifer Chukwu <jnyfaah@gmail.com>
2023-07-30 21:35:38 +0100

    Update error.rs

--- src/util/error.rs
+++ src/util/error.rs
@@ -8,7 +8,7 @@ use crate::util::location::SourceLocation;
 /// This file contains implementation of the helper logic for error handling
 /// used internally.
 /// this comment is just to trigger the build
-/// testing caching skills 3
+/// testing caching skills 4
 ///////////////////////////////////////////////////////////////////////////
 
 #[derive(Debug, Clone)]
